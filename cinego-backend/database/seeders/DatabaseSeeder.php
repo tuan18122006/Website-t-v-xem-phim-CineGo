478d6d6830f9da3c05c38f44fcea8cd9204acc38
tuan18122006 <tuan25042k6@gmail.com>
2026-06-10 23:04:21 +0700

    Fix broken poster URL for Kẻ Kiến Tạo in DatabaseSeeder

--- cinego-backend/database/seeders/DatabaseSeeder.php
+++ cinego-backend/database/seeders/DatabaseSeeder.php
@@ -77,7 +77,7 @@ class DatabaseSeeder extends Seeder
                 'description' => 'Giữa cuộc chiến khốc liệt của nhân loại và trí tuệ nhân tạo, một cựu đặc vụ được giao nhiệm vụ ám sát "Kẻ kiến tạo".',
                 'duration' => 133,
                 'release_date' => '2026-05-20',
-                'poster_url' => 'https://images.unsplash.com/photo-1478720143022-10d0002856a3?auto=format&fit=crop&w=400&q=80',
+                'poster_url' => 'https://images.unsplash.com/photo-1594909122845-11baa439b7bf?auto=format&fit=crop&w=400&q=80',
                 'rating' => 'T16',
                 'status' => 'showing'
             ])
